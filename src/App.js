a3db02731a4420065a740705f773b06f32585167
Vincent Wong <65081301+KWLON@users.noreply.github.com>
2022-01-27 21:15:52 +0000

    AddTaskSubmit

--- src/App.js
+++ src/App.js
@@ -27,6 +27,12 @@ export default function App() {
     }
   ]);
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
@@ -42,7 +48,7 @@ export default function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
