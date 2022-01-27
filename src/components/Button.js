7a881b185e50f9774b8f3d4336e1bfd7a2e6790c
Vincent Wong <65081301+KWLON@users.noreply.github.com>
2022-01-27 18:24:43 +0000

    Events

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,16 @@
 import PropTypes from "prop-types";
 
-const Button = ({ color, text }) => {
+const Button = ({ color, text, onClick }) => {
+  // const onClick = () => {
+  //   console.log('click')
+  // }
+
   return (
-    <button style={{ backgroundColor: color }} className="btn">
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
       {text}
     </button>
   );
@@ -12,9 +20,10 @@ Button.defaultProps = {
   color: "blue"
 };
 
-Button.PropTypes = {
+Button.propTypes = {
   text: PropTypes.string,
-  color: PropTypes.string
+  color: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 export default Button;
--- src/components/Headers.js
+++ src/components/Headers.js
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
+
 const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log("click");
+  };
+
   return (
     <header className="header">
       <h1 style={{ color: "grey", backgroundColor: "black" }}>{title}</h1>
-      <Button color="green" text="add" />
+      <Button color="green" text="add" onClick={onClick} />
     </header>
   );
 };
