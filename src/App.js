e8eb8ebdf3e190315bbf744d6a1a209be939df10
Vincent Wong <65081301+KWLON@users.noreply.github.com>
2022-01-27 18:42:19 +0000

    GlobalState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import "./index.css";
+import { useState } from "react";
 // import React from "react";
 import Header from "./components/Headers";
 import Tasks from "./components/Tasks";
 
 export default function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors App",
+      day: "Feb 5th at 2:30",
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "School",
+      day: "Feb 6th at 1:30",
+      reminder: true
+    },
+    {
+      id: 3,
+      text: "Shopping",
+      day: "Feb 5th at 3:30",
+      reminder: false
+    }
+  ]);
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
